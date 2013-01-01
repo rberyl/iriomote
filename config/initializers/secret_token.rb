a479869e396772a5c2fc1f5def5a8bcb430fa471
rberyl <gaura.iyra@gmail.com>
2013-01-01 02:33:28 +0000

    added std rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iriomote::Application.config.secret_token = '2cebd21aa9ee82911466f3ab04d184321f86c71581226894557035d18c69aaaaec2403538d75c948a3f9838b8b78c75c76ed3621f1d451f2d72c955ae44bbf7e'
